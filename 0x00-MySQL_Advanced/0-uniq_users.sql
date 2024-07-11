1c7188aea81d19f47e20256e139833748ea90dad
thabang maphangisane <thabangh647@gmail.com>
2024-07-11 21:16:13 +0000

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- This SQL script creates a table users with the attributes: id, email, name
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
+
