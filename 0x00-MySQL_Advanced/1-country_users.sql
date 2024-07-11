74de34be0d8bfe791ad0736ae91bbfed7b17dd4f
Thabang Maphangisane <thabangh647@gmail.com>
2024-07-12 00:10:36 +0200

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- This SQL script creates a table users with the attributes: id, email, name, country
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255) NOT NULL,
+    `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
