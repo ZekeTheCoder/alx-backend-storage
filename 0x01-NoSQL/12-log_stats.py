1c557b256d0c59a879b148e830997a674ed21f12
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-06 22:05:00 +0200

    12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,26 +7,17 @@ def collection(db: dict) -> int:
     """Function to retrieve logs information"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
-    logs_count = logs.count_documents(db)
-    client.close()  # Close the connection after performing the query
-    return logs_count
+    return logs.count_documents(db)
 
 
 def main():
     """Function that returns stats about Nginx logs stored in MongoDB"""
 
-    # total number of logs
     print(f"{collection({})} logs")
-
-    # HTTP methods to check
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    # count for each HTTP method
     print("Methods:")
     for method in methods:
         print(f"\tmethod {method}: {collection({'method': method})}")
-
-    # status check count
     print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
 
 
