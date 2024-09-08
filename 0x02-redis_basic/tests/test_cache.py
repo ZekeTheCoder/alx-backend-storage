d4be34e1f808dffad9bcb8af1cd8269b3cec405e
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-08 23:57:11 +0200

    test the call_history decorator

--- 0x02-redis_basic/tests/test_cache.py
+++ 0x02-redis_basic/tests/test_cache.py
@@ -64,6 +64,23 @@ class TestCache(unittest.TestCase):
         call_count = self.cache.get(self.cache.store.__qualname__)
         self.assertEqual(call_count, b'4')  # We called store 4 times
 
+    def test_call_history(self):
+        """Test that the call_history decorator stores inputs and outputs."""
+        # Store some values
+        s1 = self.cache.store("first")
+        s2 = self.cache.store("second")
+        s3 = self.cache.store("third")
+
+        # Check the history of inputs and outputs
+        inputs = self.cache._redis.lrange(
+            f"{self.cache.store.__qualname__}:inputs", 0, -1)
+        outputs = self.cache._redis.lrange(
+            f"{self.cache.store.__qualname__}:outputs", 0, -1)
+
+        self.assertEqual(
+            inputs, [b"('first',)", b"('second',)", b"('third',)"])
+        self.assertEqual(outputs, [s1.encode(), s2.encode(), s3.encode()])
+
 
 if __name__ == '__main__':
     unittest.main()
