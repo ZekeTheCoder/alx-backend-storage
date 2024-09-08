7ec8e86e5f48d8b0fba915bed5e0b19ed327a336
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-09 01:49:53 +0200

    5. Implementing an expiring web cache and tracker - access_count

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -26,14 +26,21 @@ def cache_decorator(method):
 @cache_decorator
 def get_page(url: str) -> str:
     """Fetch HTML content of a URL and track the number of accesses."""
-    count = redis_client.incr(f"count:{url}")
+    redis_client.incr(f"count:{url}")
     content = requests.get(url).text
-    # print(f"Access count: {count}")
     return content
 
 
-# if __name__ == "__main__":
-#     # url = "http://slowwly.robertomurray.co.uk"
-#     url = "https://httpbin.org/delay/10"
-#     print(f"First access: {get_page(url)}")
-#     print(f"Second access: {get_page(url)}")
+def access_count(url: str) -> int:
+    """Retrieve the access count for a given URL."""
+    count_key = f"count:{url}"
+    count = redis_client.get(count_key)
+    return int(count) if count else 0
+
+
+if __name__ == "__main__":
+    url = "https://httpbin.org/delay/5"
+    print(f"First access: {get_page(url)}")
+    print(f"Access count: {access_count(url)}")
+    # print(f"Second access: {get_page(url)}")
+    # print(f"Access count: {access_count(url)}")
